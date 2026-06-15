8281a302868afced95ac60dcc301fcb04bb02cd3
orh4 <127186886+orh4@users.noreply.github.com>
2026-06-15 10:22:02 +0000

    added more error handling (and added fallback messages), a config file, more comments, more readable debug messages, and cleaned the code a bit.

--- script.sh
+++ script.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
+### CONFIGURATION FILE (optional, can be done via "config.env") ###
+if [ -f "config.env" ]; then
+	source config.env
+	echo "loaded configuration from config file."
+else
+	echo "config file not found. using script defaults."
+fi
+
+
 ### VALUES ###
 
 # The default is your router's IP, but it can be changed (eg. 1.1.1.1 or google.com)
-PING_IP=192.168.1.1
-PING_WAIT_TIME=1
-PING_COUNT=5
+PING_IP="${PING_IP:-8.8.8.8}"
+PING_WAIT_TIME="${PING_WAIT_TIME:-1}"
+PING_COUNT="${PING_COUNT:-5}"
 
 # Gets the current date and time (adds it to the JSON output.)
 CURRENT_TIME=$(date +"%d/%m/%Y @ %H:%M:%S UTC")
 
 # The path to the repo/directory that will be pushed to GitHub.
-REPO_PATH="/home/your/repo/name"
+REPO_PATH="${REPO_PATH:-$HOME/your/repo/name}"
 # Path as to where the converted JSON output will be saved.
-JSON_PATH="$REPO_PATH/stats.json"
+JSON_PATH="${JSON_PATH:-$REPO_PATH/stats.json}"
 # Path as to where the badge-friendly JSON output will be saved.
-BADGES_JSON_PATH="$REPO_PATH/badges_stats.json"
+BADGES_JSON_PATH="${BADGES_JSON_PATH:-$REPO_PATH/badges_stats.json}"
 
 # The commit message the script will send out.
 COMMIT_MSG="[AUTOMATED] updated: $(date +"%Y/%m/%d @ %H:%M:%S")"
 
 ### DEBUG MESSAGES ###
 
-UPTIME_GET_MSG="getting uptime..."
-CPU_TEMP_GET_MSG="getting cpu temperature..."
-BATTERY_PERCENTAGE_GET_MSG="getting battery percentage..."
-BATTERY_TIME_LEFT_MSG="getting battery time left..."
-ENERGY_RATE_GET_MSG="getting energy-rate..."
-PING_GET_MSG="getting ping... [ip: $PING_IP, count = $PING_COUNT, wait-time = $PING_WAIT_TIME second(s)]"
+UPTIME_GET_MSG="${UPTIME_GET_MSG:-getting uptime...}"
+CPU_TEMP_GET_MSG="${CPU_TEMP_GET_MSG:-getting cpu temperature...}"
+BATTERY_PERCENTAGE_GET_MSG="${BATTERY_PERCENTAGE_GET_MSG:-getting battery percentage...}"
+BATTERY_TIME_LEFT_MSG="${BATTERY_TIME_LEFT_MSG:-getting battery time left...}"
+ENERGY_RATE_GET_MSG="${ENERGY_RATE_GET_MSG:-getting energy-rate...}"
+PING_GET_MSG="${PING_GET_MSG:-getting ping... [ip: $PING_IP, count = $PING_COUNT, wait-time = $PING_WAIT_TIME second(s)]}"
 
 ### STATS ###
 # You can comment-out stats you don't want to display.
@@ -55,8 +64,7 @@ UPTIME=$(uptime -p | cut -b 4-)
 
 # The average router ping latency (in miliseconds).
 echo $PING_GET_MSG
-LATENCY=$(ping -4 -W $PING_WAIT_TIME -c $PING_COUNT $PING_IP | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
-LATENCY="$LATENCY ms"
+LATENCY=$(ping -4 -W "$PING_WAIT_TIME" -c "$PING_COUNT" "$PING_IP" 2>/dev/null | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
 
 ### VERIFYING ###
 
@@ -75,15 +83,15 @@ for VALUE in "${STATS[@]}"
 do	
 	# Checks if the value of the variable is non-empty / nil / undefined.
 	# If value doesn't exist.
-	if [[ ! -v $VALUE ]]; then
-		echo UNDEFINED: The variable $VALUE does not exist.
+	if [[ ! -v "$VALUE" ]]; then
+		echo "- UNDEFINED: The variable $VALUE does not exist."
 		continue
 	fi
 	
 	# If variable ENERGY_RATE is equal to zero (when server in on AC power, or when it is charging.)
-	if [ $VALUE = "ENERGY_RATE" ]; then
+	if [ "$VALUE" = "ENERGY_RATE" ]; then
 		if [ "${!VALUE}" = 0 ]; then
-			echo ON AC: The variable $VALUE is zero as the device is currently plugged in.
+			echo "- ON AC: The variable $VALUE is zero as the device is currently plugged in."
 			ENERGY_RATE="[PLUGGED IN]"
 		fi
 
@@ -91,11 +99,19 @@ do
 			ENERGY_RATE="$ENERGY_RATE [PLUGGED IN]"
 		fi	
 	fi
+
+	if [ "$VALUE" = "LATENCY" ]; then
+		if [ -z "$VALUE" ]; then
+			LATENCY="offline (the specified ping-ip is not responding)."
+		else
+			LATENCY="$LATENCY ms"
+		fi
+	fi
 	
 	# If variable BATTERY_TIME_LEFT is equal to zero (when it is plugged in.)
-	if [ $VALUE = "BATTERY_TIME_LEFT" ]; then
-		if [ -z ${!VALUE} ]; then
-			echo ON AC: The variable $VALUE is nil as the device is currently plugged in.
+	if [ "$VALUE" = "BATTERY_TIME_LEFT" ]; then
+		if [ -z "${!VALUE}" ]; then
+			echo "- ON AC: The variable $VALUE is nil as the device is currently plugged in."
 			BATTERY_TIME_LEFT="[PLUGGED IN]"
 			continue
 		else
@@ -109,7 +125,7 @@ do
 		echo "${!VALUE}"
 	# If value is nil / empty.
 	else
-		echo NIL: The variable $VALUE has no value or has encountered an error.
+		echo "- NIL: The variable $VALUE has no value or has encountered an error."
 	fi
 done
 
@@ -136,36 +152,43 @@ mkdir -p "$(dirname "$JSON_PATH")"
 echo "$JSON_STRING" > "$JSON_PATH"
 echo "$JSON_STRING" > "$BADGES_JSON_PATH"
 
+# Converts the JSON output into text that is URL-friendly.
 sed -i "s|%|%25|g" $BADGES_JSON_PATH
 sed -i "s|, |%2C%20|g" $BADGES_JSON_PATH
 sed -i "s| |%20|g" $BADGES_JSON_PATH
 
 # Just echoes/outputs the specified JSON path.
-echo "saved to: $JSON_PATH"
+echo "- saved to: $JSON_PATH"
 cat $JSON_PATH
 
 ### GIT PUSH ###
 
-cat template.md > README.md
-
 cd "$REPO_PATH" || exit 1
 
-# Checks if there are any differences between the local repo/directory and the repo up on GitHub.
-# The tag "--quiet" makes the whole "git diff" return only an exit code. 0 is there are no changes, and 1 if there are changes.
-# The "!" before the command inverts the output exit code. (i.e. If there is a change in the file, it will output the exit code 1, which then gets inverted into a 0, running the commands in the below if statement.)
-# In shell, and exit code "0" marks a "true" value while an exit code "1" marks a "false" value.
-git stash --quiet
+# Outputs the contents of the template into the README (flushing out the old values and generating the template for Github Actions to fill with the new values.)
+cat template.md > README.md
+
+# Stash the unpushed changes, rebuild the repo from the remote repo, and readd the unpushed changes. (Avoids issues when dealing with multiple commits and unpushed changes.)
+git stash --quiet || echo "- nothing to stash or stash failed."
 
 git pull origin main --rebase --quiet
 
 git stash pop --quiet
 
+# Checks if there are any differences between the local repo/directory and the repo up on GitHub.
+# The tag "--quiet" makes the whole "git diff" return only an exit code. 0 is there are no changes, and 1 if there are changes.
+# The "!" before the command inverts the output exit code. (i.e. If there is a change in the file, it will output the exit code 1, which then gets inverted into a 0, running the commands in the below if statement.)
+# In shell, and exit code "0" marks a "true" value while an exit code "1" marks a "false" value.a
+
 if ! git diff --quiet stats.json; then
-	# Adds the given output JSON into the commit.
+	# Adds the given JSON output(s) into the commit.
 	git add stats.json
 	git add badges_stats.json
-
+	
+	# Outputs the contents of the template into the README (flushing out the old values and generating the template for Github Actions to fill with the new values.)
 	cat template.md > README.md
+
+	# Adds the changed README into the commit.
 	git add README.md
 
 	# Adds the configured commit message (configured via setting the "COMMIT_MSG" variable.)
@@ -173,11 +196,12 @@ if ! git diff --quiet stats.json; then
 	
 	# Pushes the commit to GitHub from origin (local files) to main (main branch on GitHub) and specifies where it got pushed to.
 	if git push origin main 2>&1; then
-		echo "pushed to configured github repo."
+		echo "- pushed to configured github repo."
 	else
-		echo "push failed. remote may have new commits."
+		echo "- push failed. remote may have new commits."
 	fi
 else
-	echo "no changes to commit."
+	# If there are no differences between the local repo and the remote repo, no commit will be pushed.
+	echo "- no changes to commit."
 fi
 
